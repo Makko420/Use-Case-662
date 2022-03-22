fe2d8392f24939dd3d82a4c69d0db6c829d7c34a
Makko420 <laidinen.marco@gmail.com>
2022-03-22 12:33:45 +0200

    lisäsin js korjasin css

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 //hides poll-1
 var x = document.getElementById("poll-1");
 x.style.display = "none";
-  
-//function addAnswer() {
-//    let spare = document.getElementById(lastID).value;
-//    if (spare != "") {
-//    }
-//}
+
+//set multiVoting to "false" to vote only once or to "true" to vote multiple times
+let multiVoting = true;
+let voted = false;
+let pollCreated = false;
 
 class TestPoll {
     constructor(question, answer = [], results = []) {
@@ -33,24 +32,37 @@ class TestPoll {
 }
 
 function createPoll() {
-    //unhides poll-1
-    x.style.display = "block";
+    if (pollCreated == false) {
+        //unhides poll-1
+        x.style.display = "block";
 
-    //adds question to TestPoll
-    myPoll.addQuestion(document.getElementById("question").value);
+        //adds question to TestPoll class
+        myPoll.addQuestion(document.getElementById("question").value);
 
-    for(i = 1; i <= 4; i++) {
-        //get answers
-        let spare = document.getElementById(i).value;
-        myPoll.addAnswers(spare, i);
-        document.getElementById("p1:" + i).innerHTML = spare;
+        for(i = 1; i <= 4; i++) {
+            //gets the answers and adds them to TestPoll class
+            let spare = document.getElementById(i).value;
+            myPoll.addAnswers(spare, i);
+            document.getElementById("p1:" + i).innerHTML = spare;
+        }
+        pollCreated = true;
+    } else {
+        console.log("A poll has been created already")
     }
-
 }
 
 function vote(i) {
-    let votes = myPoll.vote(i-1);
-    document.getElementById("p1r:" + i).innerHTML = votes;
+    if (multiVoting == true) {
+        let votes = myPoll.vote(i-1);
+        document.getElementById("p1r:" + i).innerHTML = votes;
+        console.log("You have voted with multi vote");
+    } else if (voted == false) {
+        let votes = myPoll.vote(i-1);
+        document.getElementById("p1r:" + i).innerHTML = votes;
+        voted = true;
+    } else {
+        console.log("You have voted already");
+    }
 }
 
 function debug() {
